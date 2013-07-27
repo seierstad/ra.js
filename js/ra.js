d97d7449c9b584656c1913e8189c0cc39e71916e
seierstad <erik.seierstad@gmail.com>
2013-07-27 02:04:16 +0200

    added require.js

--- js/ra.js
+++ js/ra.js
@@ -4,7 +4,70 @@
  * Date: 2013-07-24
  * Time: 00:19
  */
-(function () {
+(function (global) {
     "use strict";
-    console.log('hello ');
-}());
+
+	// export either as a CommonJS module or a global variable
+
+	var Ra;
+
+	if (typeof exports !== 'undefined') {
+		Ra = exports;
+	} else {
+		Ra = global.Ra = {};
+	}
+
+    Ra.position = {};
+    Ra.movement = {};
+    Ra.orientation = {};
+
+    function positionCallback (pos) {
+    	Ra.position = pos;
+	}
+
+	function getLocation () {
+	  navigator.geolocation.getCurrentPosition(positionCallback);
+	}
+	
+	function motionHandler (event) {
+		Ra.movement.x = event.accelerationIncludingGravity.x;
+		Ra.movement.y = event.accelerationIncludingGravity.y;
+		Ra.movement.z = event.accelerationIncludingGravity.z;
+
+	}
+
+	function orientationHandler (event) {
+	    var absolute, alpha, beta, gamma;
+	    absolute = event.absolute;
+	    alpha = event.alpha;
+	    beta = event.beta;
+	    gamma = event.gamma;
+
+	    Ra.orientation = event;
+	}
+
+	var orientationchangelistener = function (event) {
+		Ra.orientation.screen = (screen.orientation || screen.webkitOrientation || screen.mozOrientation || window.orientation);
+	};
+
+	Ra.stringifyObject = function (o) {
+	  var p, s = ''; 
+	  for (p in o) {
+	  	if (o.hasOwnProperty(p)) {
+	  		if (typeof o[p] !== 'object' || o[p] === null) {
+	  			s += p + ': ' + o[p] + '\n';
+	  		} else {
+	  			s += '\n' + p + ': \n' + this.stringifyObject(o[p]) + '\n';
+	  		}
+	  	}
+	  }
+	  return s;
+	};
+
+	global.addEventListener('devicemotion', motionHandler, false);
+	global.addEventListener('deviceorientation', orientationHandler, false);
+	global.addEventListener('orientationchange', orientationchangelistener, false);
+	global.addEventListener('load', getLocation, false);
+
+
+}(this));
