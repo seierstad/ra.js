65b324d5c0af8d1ee7912da07d8bb56a53258285
seierstad <erik.seierstad@gmail.com>
2013-07-29 22:21:24 +0200

    require.js working

--- scripts/ra.js
+++ scripts/ra.js
@@ -6,13 +6,13 @@
  */
 (function (global) {
     "use strict";
-	require(["../lib/suncalc/suncalc"], function(util) {
+	require(["suncalc/suncalc"], function(sc) {
 	    //This function is called when scripts/helper/util.js is loaded.
 	    //If util.js calls define(), then this function is not fired until
 	    //util's dependencies have loaded, and the util argument will hold
 	    //the module value for "helper/util".
+	    console.log('suncalc loaded');
 	});
-	// export either as a CommonJS module or a global variable
 
 	var Ra;
 
